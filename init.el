20ec6f21333cd0f52dee2d19206ccecf97b9e98c
y2k <itwisterlx@gmail.com>
2023-01-04 15:38:57 +0400

    update up/down

--- init.el
+++ init.el
@@ -3,17 +3,21 @@
 
 (xterm-mouse-mode)
 
-(require 'lsp-mode)
-(global-set-key (kbd "M-,") 'complete-symbol)
-(global-set-key (kbd "M-1") 'treemacs)
-(global-set-key (kbd "C-l") 'lsp-format-buffer)
-(require 'expand-region)
-(global-set-key (kbd "C-<up>") 'er/expand-region)
-
 (add-hook 'clojurescript-mode-hook #'paredit-mode)
 (add-hook 'clojure-mode-hook #'paredit-mode)
 (add-hook 'fsharp-mode-hook #'lsp-mode)
 (add-hook 'tuareg-mode-hook #'lsp-mode)
+
+(require 'lsp-mode)
+(global-set-key (kbd "M-,") 'complete-symbol)
+(global-set-key (kbd "M-1") 'treemacs)
+(global-set-key (kbd "C-l") 'lsp-format-buffer)
+
+(require 'expand-region)
+(define-key input-decode-map "\e\eOA" [(meta up)])
+(define-key input-decode-map "\e\eOB" [(meta down)])
+(global-set-key (kbd "M-<up>") 'er/expand-region)
+(global-set-key (kbd "M-<down>") 'er/contract-region)
 
 ;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
 ;; (require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
