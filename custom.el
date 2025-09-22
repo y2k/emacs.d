5ecbcfc04f64a078b17540d35160994307bdfb89
y2k <itwisterlx@gmail.com>
2025-09-22 17:56:14 +0000

    clean code

--- custom.el
+++ custom.el
@@ -3,9 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(company rescript-mode expand-region bind-key use-package queue undo-tree all-the-icons treemacs paredit tuareg lsp-mode ## fsharp-mode clojure-mode doom-themes))
- '(warning-suppress-types '((lsp-mode))))
+ '(package-selected-packages '(clojure-mode lsp-mode treemacs undo-tree)))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -1,11 +1,8 @@
 (load "~/.emacs.d/sanemacs.el" nil t)
-(load "~/.emacs.d/doom-theme-config.el" nil t)
 
-(xterm-mouse-mode)
+(load-theme 'tango-dark t)
 
-(add-hook 'clojurescript-mode-hook #'paredit-mode)
 (add-hook 'clojure-mode-hook #'paredit-mode)
-(add-hook 'fsharp-mode-hook #'lsp-mode)
 (add-hook 'tuareg-mode-hook #'lsp-mode)
 
 (require 'lsp-mode)
@@ -13,22 +10,3 @@
 (global-set-key (kbd "M-1") 'treemacs)
 (global-set-key (kbd "C-l") 'lsp-format-buffer)
 
-(require 'expand-region)
-(load "~/.emacs.d/local.init.el" nil t)
-(global-set-key (kbd "M-<up>") 'er/expand-region)
-(global-set-key (kbd "M-<down>") 'er/contract-region)
-
-(defun duplicate-line()
-  (interactive)
-  (move-beginning-of-line 1)
-  (kill-line)
-  (yank)
-  (open-line 1)
-  (next-line 1)
-  (yank)
-)
-(global-set-key (kbd "C-d") 'duplicate-line)
-
-;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
-;; (require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
-;; ## end of OPAM user-setup addition for emacs / base ## keep this line
