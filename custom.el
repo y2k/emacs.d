ef16465ca963744d9f7a6b8f96272eaee334f24a
y2k <itwisterlx@gmail.com>
2025-09-22 18:03:47 +0000

    clean code

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(clojure-mode lsp-mode treemacs undo-tree)))
+ '(package-selected-packages '(clojure-mode lsp-mode paredit treemacs tuareg undo-tree)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
